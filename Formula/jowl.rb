f0deaeba43e2d2848128ba2fba02953c4b2f6d5d
Daniel Axelrod <git@danonline.net>
2022-01-16 21:03:24 -0500

    Good syntax

--- Formula/jowl.rb
+++ Formula/jowl.rb
@@ -22,6 +22,6 @@ class Jowl < Formula
     # Until we have a better strategy for running the integration suite here,
     # this sample from that suite is unlikely to ever change and is a reasonable
     # coherency check.
-    pipe_output("#{bin}/jowl 'd[0]'", '["one", "two"]', '"wrong"\n', 0)
+    assert_match "wrong\n", pipe_output("#{bin}/jowl 'd[0]'", '["one", "two"]', 0)
   end
 end
